af2fcfd5385d7365b8af8c69ac72a6231be721cb
kurisu-seima <kurisuseima@gmail.com>
2021-08-05 21:22:05 +0900

    修正

--- Sources/Seima/Seima.swift
+++ Sources/Seima/Seima.swift
@@ -1,8 +1,8 @@
 public struct Seima {
     public var age: Int = 22
     
-    public func sayGreeting() {
-        print("ぴえん🥺")
+    public func sayGreeting() -> String {
+        return "ぴえん🥺"
     }
     public init() {}
 }
